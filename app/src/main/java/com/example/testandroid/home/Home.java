6661c2f358abdf291b2a8d84d7a64e0b803ed426
dhruv151104 <85018528+dhruv151104@users.noreply.github.com>
2023-04-22 21:26:21 +0530

    updated ui func.. in aryan branch

--- app/src/main/java/com/example/testandroid/home/Home.java
+++ app/src/main/java/com/example/testandroid/home/Home.java
@@ -3,4 +3,9 @@ package com.example.testandroid.home;
 public class Home {
     // Toast.makeText("MainActivity.this","Home page",Toast.LENGTH_SHORT).show()
 //    Home page created
+
+
+
+    ///changes added by dhruv
+
 }
